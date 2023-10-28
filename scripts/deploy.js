17921870654f8e79080f7dd95a9f7d1b50d2dd20
Harman-singh-waraich <Waraichharman68@gmail.com>
2023-10-28 19:31:41 +0530

    make deployment easier

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,23 +1,30 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+require("dotenv").config();
 
 async function main() {
   const inheritAmount = hre.ethers.parseEther("0.001");
-  const heir = "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db";
+  const heir = process.env.HEIR; //add heir here
+
   const inherit = await hre.ethers.deployContract("Inheritance", [heir], {
     value: inheritAmount,
   });
 
   await inherit.waitForDeployment();
+
+  const address = await inherit.target;
+
+  console.log("Contract deployed to", address);
+
+  console.log("Waiting 5 confirmations before verifying");
+
+  await inherit.deploymentTransaction()?.wait(5);
+
+  await hre.run("verify:verify", {
+    address: inherit.target, //contract address
+    constructorArguments: [heir],
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
